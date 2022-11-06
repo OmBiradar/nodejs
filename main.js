78ccb799aa2eef08a6326674bad0964d980408c9
Om Biradar <ombiradar04@gmail.com>
2022-11-06 08:43:21 +0000

    Create main.js

---
+++ main.js
@@ -0,0 +1,9 @@
+const a = process.argv[2];
+
+const reverseStr = str.split().reverse().join('');
+
+if (reversedStr === str){
+    console.log(a + ' is a palindrome');
+}else {
+    console.log(a + ' is not palindrome');
+}
